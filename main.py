db8c9693c51d7a39af0e3cf554532902c8843b71
BlueWood1425 <nika.konyk@gmail.com>
2023-06-02 19:31:16 +0300

    I have add an exception

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 result = []
 
-
 def divider(a, b):
     if a < b:
-        raise ValueError
+        raise ValueError("ValueError: a < b")
     if b > 100:
-        raise IndexError
+        raise IndexError("IndexError: b > 100")
     return a / b
 
+data = {10: 2, 2: 5, "123": 4, 18: 0, (): 15, 8: 4}
 
-data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
 for key in data:
-    res = divider(key, data[kem])
-    result.append(res)
+    try:
+        res = divider(key, data[key])
+        result.append(res)
+    except ValueError as e:
+        print(e)
+    except IndexError as e:
+        print(e)
+    except Exception as e:
+        print("Exception:", e)
 
 print(result)
